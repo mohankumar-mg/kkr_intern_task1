4ea8bdd0502243263a1fd2a6c985a155f6396ac4
mohankumar-mg <mohankumargandhi09@gmail.com>
2024-10-04 12:39:47 +0530

    animations added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="shadow-lg shadow-cyan-500/50 w-[260px] h-[350px] rounded-xl">
-        <h1 className="font-bold font-mono text-xl text-center pt-[10px] text-indigo-500">{props.service}</h1>
+    <div className="shadow-lg shadow-cyan-500/50 w-[260px] h-[310px] rounded-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
+        <h1 className="font-bold font-mono text-xl text-center pt-[20px] hover:text-indigo-500">{props.service}</h1>
         <p className="p-6 text-center text-lg font-serif">&nbsp;&nbsp;{props.description}</p>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ function Navbar() {
   return (
     <div className="flex p-[10px] justify-start">
       <img src={Logo} alt="logo" className="h-[100px] w-[100px] mr-[70%] bg-black rounded-2xl" />
-      <h1 className="text-xl font-semibold mr-[50px] text-black hover:text-indigo-500 cursor-pointer pt-[10px]">
+      <h1 className="text-xl font-semibold mr-[50px] hover:text-indigo-500 cursor-pointer pt-[10px]" onClick={()=> window.scrollTo(0, 0)}>
         About
       </h1>
-      <h1 className="text-xl font-semibold mr-[50px] text-black hover:text-indigo-500 cursor-pointer pt-[10px]">
+      <h1 className="text-xl font-semibold mr-[50px] hover:text-indigo-500 cursor-pointer pt-[10px]" onClick={()=> window.scrollTo(0, 300)}>
         Services
       </h1>
-      <h1 className="text-xl font-semibold mr-[50px] text-black hover:text-indigo-500 cursor-pointer pt-[10px]">
+      <h1 className="text-xl font-semibold mr-[50px] hover:text-indigo-500 cursor-pointer pt-[10px]" onClick={()=> window.scrollTo(0, 300)}>
         Contact
       </h1>
     </div>
